5994af1bba2c2bc1b4e40841c9e1b3c018a8eb4d
Sergio <Sergio@Sergio-PC>
2018-03-07 15:28:10 -0500

    encasa

---
+++ Parqueadero/src/Vehiculos/Celador.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vehiculos;
+
+/**
+ *
+ * @author Sergio
+ */
+public class Celador {
+
+    String saludarhombre() {
+        return "Hola doctor";
+
+    }
+
+    String saludarmujer() {
+        return ("Me gustaría ser rana asustada, para esconderme todo el día en tu zanja mojada.");
+    }
+
+    String sobrio() {
+        return ("Bienvenido sea, tenga buen dia");
+    }
+
+    String borracho() {
+        return ("vemos de aca o me lo llevo");
+    }
+
+    String gafas() {
+        return ("No olvide llevar sus gafas");
+    }
+
+}
